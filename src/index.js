f45afe5298a7fbbc5a7918b8ce7dda73c9d3b2f1
Steve Hansen <shansen@dbe.bz>
2017-11-29 16:57:40 -0600

    Part of Section 29

--- src/index.js
+++ src/index.js
@@ -11,17 +11,23 @@ class App extends Component {
     constructor(props) { 
         super(props);
 
-        this.state = { videos: [] };
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        };
 
         YTSearch({key:API_KEY, term: 'surfboards'}, (videos) => {
-            this.setState({ videos }); //this.setState({ videos: videos });
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0] 
+            }); //this.setState({ videos: videos });
         });
     }
     render () {
         return(
             <div>
                 <SearchBar />
-                <VideoDetail video={this.state.videos[0]}/>
+                <VideoDetail video={this.state.selectedVideo}/>
                 <VideoList videos={this.state.videos} />
             </div>
         );   
