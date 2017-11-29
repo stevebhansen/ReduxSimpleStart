7dc6e7e6756ea1bd0804fe89afc0519752399ccf
Steve Hansen <shansen@dbe.bz>
2017-11-29 13:59:00 -0600

    add constructor

---
+++ src/components/search_bar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };
+    }
+    render() {
+        return <input onChange={ event => console.log(event.target.value) } />;
+    }
+    /*
+    onInputChange(event) {
+        console.log(event.target.value);
+    }
+    */
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyAv1Ny8AyzWbGISQ4890FjimbPZVXYhx6A';
 
 const App = () => {
-    return <div>Hi!</div>
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
