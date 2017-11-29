830b854d12ea17eb320a918d2474f9ecbbbdc770
Steve Hansen <shansen@dbe.bz>
2017-11-29 16:04:37 -0600

    section 2

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyAv1Ny8AyzWbGISQ4890FjimbPZVXYhx6A';
 
-YTSearch({key:API_KEY, term: 'surfboards'}, function(data) {
-    console.log(data);
-});
+class App extends Component {
+    constructor(props) { 
+        super(props);
 
-const App = () => {
+        this.state = { videos: [] };
+
+        YTSearch({key:API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos }); //this.setState({ videos: videos });
+        });
+    }
     return (
         <div>
             <SearchBar />
